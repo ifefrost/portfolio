9b65a8c86199a4a2d7c25e00bb956fe6128407b9
ifefrost <temiladeladipo@hotmail.com>
2023-04-20 17:59:42 -0400

    step one favicon and setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return <div className='text-3xl font-bold'>Start coding!</div>;
+}
+export default App;
