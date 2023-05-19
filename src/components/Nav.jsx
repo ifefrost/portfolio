e13e6cad16dc8288df5a5a76203686a927da8bdb
ifefrost <temiladeladipo@hotmail.com>
2023-05-19 00:16:40 -0400

    mobile responsive fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
-import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { MdLightMode, MdDarkMode, MdMenu } from "react-icons/md";
 import { useState } from "react";
 
 const Nav = () => {
   const navigate = useNavigate();
+  const [navOpen, setNavOpen] = useState(false);
   const [theme, setTheme] = useState(true);
   const toggleTheme = () => {
     setTheme(!theme);
@@ -19,31 +20,57 @@ const Nav = () => {
           >
             IfeLadipo
           </Link>
-          <div className='flex space-x-4'>
-            <Link
-              to='/work'
-              className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
-            >
-              Work
-            </Link>
-            <Link
-              to='/about'
-              className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
-            >
-              About
-            </Link>
-            <h1 className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'>
-              Resume
-            </h1>
-            <h1 className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'>
-              Connect
-            </h1>
-            <button
-              onClick={toggleTheme}
-              className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
-            >
-              {theme ? <MdLightMode /> : <MdDarkMode />}
-            </button>
+          <button
+            type='button'
+            className={`focus:outline-none ${
+              navOpen
+                ? "bg-rose-700 text-white"
+                : "text-gray-300 hover:bg-rose-900 hover:text-white"
+            } sm:hidden order-last cursor-pointer relative 
+                  px-3 py-2 rounded-md text-sm font-medium `}
+            onClick={() => setNavOpen(!navOpen)}
+          >
+            <MdMenu className='h-8 w-8' />
+          </button>
+          <div
+            className={`${navOpen ? "block" : "hidden"} sm:flex sm:space-x-4`}
+          >
+            <div className='flex flex-col sm:flex-row sm:space-x-4 absolute sm:static right-2 top-11 mt-5 sm:mt-0 bg-rose-900 sm:bg-inherit px-1 py-2 items-center sm:gap-0 rounded-b-xl w-max'>
+              <Link
+                to='/work'
+                className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
+              >
+                Work
+              </Link>
+              <Link
+                to='/about'
+                className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
+              >
+                About
+              </Link>
+              <h1 className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'>
+                Resume
+              </h1>
+              <h1 className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'>
+                Connect
+              </h1>
+              <button
+                onClick={toggleTheme}
+                className='text-center w-full text-black hover:bg-gray-100 hover:text-rose-700 px-3 py-2 rounded-md text-sm font-medium'
+              >
+                {theme ? (
+                  <div className="flex gap-2 items-center">
+                    <span className='sm:sr-only'>Set Light Mode</span>
+                    <MdLightMode />
+                  </div>
+                ) : (
+                  <div className="flex gap-2 items-center">
+                    <span className='sm:sr-only'>Set Dark Mode</span>
+                    <MdDarkMode />
+                  </div>
+                )}
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className='bg-rose-700 rounded-b-[32px] h-[40rem] flex gap-8 items-center px-24 overflow-hidden'>
         <div className='flex-1 flex flex-col gap-5'>
           <p className='text-white text-lg'>Hello, I'm Ife John Ladipo</p>
-          <h3 className='lg:text-[3rem] md:text-[3rem] text-[2.5rem] leading-none font-bold text-white'>
+          <h3 className='lg:text-[3rem] md:text-[2.5rem] text-[2rem] leading-none font-bold text-white'>
             A <em>versatile</em> software engineer with a{" "}
             <em>keen eye for detail</em> and a <em>dedication</em> to{" "}
             <em>clean, efficient code</em>.
@@ -14,18 +14,18 @@ const Home = () => {
           <Social />
           <div className='flex gap-10'>
             <a
-              className='cursor-pointer border-white border-2 hover:bg-white hover:text-black text-white font-bold py-3 px-8 rounded-full hover:shadow'
+              className='cursor-pointer border-white border-2 hover:bg-white hover:text-black text-white text-center font-bold p-2 md:py-3 md:px-8 rounded-xl lg:rounded-full hover:shadow'
             >
               My Work Info
             </a>
             <a
-              className='cursor-pointer border-white border-2 hover:bg-white hover:text-black text-white font-bold py-3 px-8 rounded-full hover:shadow'
+              className='cursor-pointer border-white border-2 hover:bg-white hover:text-black text-white text-center p-2 font-bold md:py-3 md:px-8 rounded-xl lg:rounded-full hover:shadow'
             >
               More About Me
             </a>
           </div>
         </div>
-        <div className='rounded-full bg-[#fdb813] flex-1 mt-auto'>
+        <div className='rounded-full bg-[#fdb813] flex-1 mt-auto hidden md:block'>
           <img src={profile} alt='side profile of ife' className='' />
         </div>
       </div>
